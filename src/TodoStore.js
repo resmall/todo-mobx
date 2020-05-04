65047676cf42621da056619e139b90a108ffa281
Tiago R. Lammers <trlammers@gmail.com>
2020-05-04 11:27:04 -0300

    first

---
+++ src/TodoStore.js
@@ -0,0 +1,10 @@
+import { observable } from "mobx";
+
+class TodoStore {
+  @observable todos: ["buy milk", "buy eggs"];
+  @observable filter = "";
+}
+
+var store = (window.store = new TodoStore());
+
+export default store;
